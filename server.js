a5a9b87b987b248f9f3005777c9f28475fdff62c
Abdirahman <abdirahmandubow4@gmail.com>
2025-02-13 16:08:58 +0800

    updating server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/doctor", doctorRoutes); 
+app.use("/uploads", express.static("uploads"));
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
